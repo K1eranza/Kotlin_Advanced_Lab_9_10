7604bbf7062d4ea307079245dcaa347a90635ee6
pomarulit007-cyber <poma.rulit007@gmail.com>
2026-01-14 11:33:25 +0300

    izm

--- src/Hero.kt
+++ src/Hero.kt
@@ -3,19 +3,23 @@ class Hero(val name: String) {
         set(value) {
             field = value.coerceIn(0, 100)
         }
+
     var stamina: Int = 50
         get() = field + 10
+
     var level: Int = 1
         private set
+
     fun levelup() {
         level++
         println("$name повысил уровень до $level")
     }
-    fun main() {
-        val hero = Hero(name = "Утер")
-        hero.health = 150
-        println(hero.health)
-        println(hero.stamina)
-        hero.levelup()
-    }
+}
+
+fun main() {
+    val hero = Hero(name = "Утер")
+    hero.health = 150
+    println(hero.health)
+    println(hero.stamina)
+    hero.levelup()
 }
